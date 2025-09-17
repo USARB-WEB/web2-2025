eaa4dd4b1310716613cfe4ba3de50643c8cead8d
sergiuchilat <chilatsergiu@gmail.com>
2025-09-17 10:03:20 +0300

    Add TypeORM and Dockerfile

--- nestjs-app/src/app.module.ts
+++ nestjs-app/src/app.module.ts
@@ -1,10 +1,30 @@
-import { Module, Post } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './modules/posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [PostsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('POSTGRES_HOST'),
+        port: config.get<number>('POSTGRES_PORT'),
+        username: config.get<string>('POSTGRES_USER'),
+        password: config.get<string>('POSTGRES_PASSWORD'),
+        database: config.get<string>('POSTGRES_DB'),
+        autoLoadEntities: true,
+        synchronize: true, // ⚠️ dev only
+      }),
+    }),
+    PostsModule
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
