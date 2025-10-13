81355e0812db2106da70aed738b4fd1eb85d0829
sergiuchilat <chilatsergiu@gmail.com>
2025-10-13 09:11:36 +0300

    Middleware and authentication

--- nestjs-app/src/modules/students/students.module.ts
+++ nestjs-app/src/modules/students/students.module.ts
@@ -1,12 +1,20 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { StudentsService } from './students.service';
 import { StudentsController } from './students.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Student } from './entities/student.entity';
+import { StudyGroup } from '../study_groups/entities/study_group.entity';
+import { LoggerMiddleware } from '../../middlewares/logger.middleware';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Student])],
+  imports: [TypeOrmModule.forFeature([Student, StudyGroup])],
   controllers: [StudentsController],
   providers: [StudentsService],
 })
-export class StudentsModule {}
+export class StudentsModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes(StudentsController);
+  }
+}
